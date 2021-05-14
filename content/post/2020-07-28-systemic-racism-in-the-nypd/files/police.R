939105097f602f404afd833c6e9b3eaac41b290d
James P. Hare <james@jamesphare.org>
2021-05-14 08:25:08 -0400

    Moved R code to separate script

--- content/post/2020-07-28-systemic-racism-in-the-nypd/files/police.R
+++ content/post/2020-07-28-systemic-racism-in-the-nypd/files/police.R
@@ -2,15 +2,19 @@ library(tidyverse)
 library(sf)
 
 #Police precinct map from NYC Open Data https://data.cityofnewyork.us/Public-Safety/Police-Precincts/78dh-3ptz#revert
-nyc_precincts <- read_sf("~/Documents/2020/nypd_allegations/data/Police Precincts/geo_export_cfbe857e-58ec-4848-b99b-1f33fd967621.shp")
+nyc_precincts <-
+        read_sf(
+                "~/Documents/2020/nypd_allegations/data/Police Precincts/geo_export_cfbe857e-58ec-4848-b99b-1f33fd967621.shp"
+        )
 
 #Alleged misconduct from ProPublica
-allegations <- read_csv("~/Documents/2020/nypd_allegations/data/allegations_20200726939.csv")
+allegations <-
+        read_csv("~/Documents/2020/nypd_allegations/data/allegations_20200726939.csv")
 
 #Let's limit this to substantiated allegations
 allegations <- allegations %>%
         filter(board_disposition != "Unsubstantiated" &
-                       board_disposition!= "Exonerated")
+                       board_disposition != "Exonerated")
 
 precinct_allegations <- allegations %>%
         filter(precinct < 124 & precinct > 0) %>%
@@ -23,16 +27,18 @@ ggplot(precinct_allegations) +
         labs(title = "Substantiated NYPD Misconduct by Precinct, 1985 to 2020",
              subtitle = "Miscondunct affects some neighborhoods more than others",
              caption = "Source: ProPublica") +
-        theme(legend.position = c(0, 1),
-              legend.justification = c(-.4, 1.1),
-              legend.title = element_blank(),
-              axis.text.x = element_blank(),
-              axis.ticks.x = element_blank(),
-              axis.text.y = element_blank(),
-              axis.ticks.y = element_blank(),
-              panel.grid.major = element_blank(), 
-              panel.grid.minor = element_blank(),
-              panel.background = element_blank()) +
+        theme(
+                legend.position = c(0, 1),
+                legend.justification = c(-.4, 1.1),
+                legend.title = element_blank(),
+                axis.text.x = element_blank(),
+                axis.ticks.x = element_blank(),
+                axis.text.y = element_blank(),
+                axis.ticks.y = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank()
+        ) +
         scale_fill_continuous(type = "viridis")
 
 ggsave("content/post/2020-07-28-systemic-racism-in-the-nypd/files/plot1.png")
@@ -55,19 +61,23 @@ remove(cops)
 
 ggplot(ethnicity) +
         geom_bar(aes(ethnicity, fill = ethnicity)) +
-        facet_wrap(~ type, scales = "free_x") +
+        facet_wrap( ~ type, scales = "free_x") +
         coord_flip() +
-        labs(title = "Substantiated NYPD Misconduct by Ethnicity, 1985 to 2020",
-             subtitle = "White police engage in most misconduct, most of their victims are Black",
-             caption = "Does not include cases where ethnicity is unkown
+        labs(
+                title = "Substantiated NYPD Misconduct by Ethnicity, 1985 to 2020",
+                subtitle = "White police engage in most misconduct, most of their victims are Black",
+                caption = "Does not include cases where ethnicity is unkown
              Source: ProPublica",
-             x = NULL,
-             y = NULL) +
-        theme(panel.grid.major = element_blank(), 
-              panel.grid.minor = element_blank(),
-              panel.background = element_blank(),
-              axis.ticks.y = element_blank(),
-              legend.position = "none") +
+                x = NULL,
+                y = NULL
+        ) +
+        theme(
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(),
+                axis.ticks.y = element_blank(),
+                legend.position = "none"
+        ) +
         scale_fill_brewer()
 
 ggsave("content/post/2020-07-28-systemic-racism-in-the-nypd/files/plot2.png")
@@ -93,7 +103,7 @@ allegations %>%
               legend.position = "none") +
         scale_fill_brewer()
 
-ggsave("plot3.png")
+ggsave("content/post/2020-07-28-systemic-racism-in-the-nypd/files/plot3.png")
 
 allegations %>%
         filter(!(is.na(complainant_ethnicity)),
@@ -118,4 +128,4 @@ allegations %>%
               legend.position = "none") +
         scale_fill_brewer()
 
-ggsave("content/post/2020-07-28-systemic-racism-in-the-nypd/files/plot3.png")
+ggsave("content/post/2020-07-28-systemic-racism-in-the-nypd/files/plot4.png")
